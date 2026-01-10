94fee5c119544891f8abead2c4bd3d40fbeb4435
Jan-Marvin <69450869+Jan-Marvin@users.noreply.github.com>
2026-01-10 14:09:52 +0100

    Add adjustable delay and PaperMC and forks now working

--- src/main/java/simpleautofishing/simpleautofishing.java
+++ src/main/java/simpleautofishing/simpleautofishing.java
@@ -1,70 +1,178 @@
 package simpleautofishing;
 
-import org.slf4j.Logger;
-
-import com.mojang.logging.LogUtils;
-
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.Items;
+import com.mojang.logging.LogUtils;
+
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
-import simpleautofishing.mixin.FishingBobberEntityAccessorMixin;
+import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
+import org.slf4j.Logger;
 
-import static simpleautofishing.simpleautofishingMode.fishingRod;
+import simpleautofishing.mixin.FishingBobberEntityAccessorMixin;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(simpleautofishing.MODID)
-public class simpleautofishing
-{
+public class simpleautofishing {
 
     public static final String MODID = "simpleautofishing";
     public static final Logger LOGGER = LogUtils.getLogger();
-    private int delay = 15;
-    private boolean hookCastOut = true;
+    private static Minecraft client;
+    boolean reeledIn, stateAttackKeyReleased = false;
+    FishingRodModes FishingRodMode = FishingRodModes.fishingRodUnprotected;
+    private int delay = 0;
+    public static int recastDelayTicks = 17;
+    enum FishingRodModes {
+        fishingRodUnprotected,
+        fishingRodProtected,
+        allInHotbar;
+
+        public FishingRodModes next() {
+            return values()[(ordinal() + 1) % values().length];
+        }
+    };
 
     public simpleautofishing() {
         LOGGER.info("Register simpleautofishing");
         NeoForge.EVENT_BUS.register(this);
-
     }
 
     @SubscribeEvent
-    public void onTick(ClientTickEvent.Post event) {
-        if (Minecraft.getInstance().player == null) {
+    public void RegisterClientCommandsEvent(RegisterClientCommandsEvent event) {
+        CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
+        dispatcher.register(net.minecraft.commands.Commands.literal("saf")
+                .then(net.minecraft.commands.Commands.literal("set")
+                        .then(net.minecraft.commands.Commands.argument("delay", IntegerArgumentType.integer())
+                                .executes(context -> {
+                                    recastDelayTicks = IntegerArgumentType.getInteger(context, "delay");
+                                    context.getSource().sendSuccess(
+                                            () -> Component.translatable(
+                                                    "text.simpleautofishing.cmd.recastDelayTicks",
+                                                    recastDelayTicks
+                                            ),
+                                            false
+                                    );
+                                    return 1;
+                                })
+                        )
+                )
+        );
+    }
+
+    @SubscribeEvent
+    public void ClientTickEvent(ClientTickEvent.Pre event)  {
+        if (Minecraft.getInstance() == null) {
+            return;
+        } else if (Minecraft.getInstance() != null && client == null) {
+            client = Minecraft.getInstance();
+        }
+
+        if (client.player == null) {
             return;
         }
 
-        simpleautofishingMode.ModeChanger();
-
-        if (Minecraft.getInstance().player.fishing == null && !hookCastOut && delay == 15) {
-            UseRod();
-            hookCastOut = true;
-        } else if (Minecraft.getInstance().player.fishing == null && !hookCastOut && delay != 15) {
-            delay++;
+        if (!isFishingRodEquipped()) {
+            delay = 0;
+            reeledIn = false;
+            return;
         }
 
-        if (Minecraft.getInstance().player.fishing != null && ((FishingBobberEntityAccessorMixin) Minecraft.getInstance().player.fishing).getBiting()) {
-            UseRod();
+        if (client.player.isCrouching() && attackKeyReleased(client.options.keyAttack.isDown())) {
+            FishingRodMode = FishingRodMode.next();
+            if (FishingRodMode == FishingRodModes.fishingRodUnprotected) {
+                client.player.displayClientMessage(Component.translatable("text.simpleautofishing.safMode.fishing_rod_unprotected"), true);
+            } else if (FishingRodMode == FishingRodModes.fishingRodProtected) {
+                client.player.displayClientMessage(Component.translatable("text.simpleautofishing.safMode.fishing_rod_protected"), true);
+            } else if (FishingRodMode == FishingRodModes.allInHotbar) {
+                client.player.displayClientMessage(Component.translatable("text.simpleautofishing.safMode.all_in_hotbar"), true);
+            }
+        }
+        if (client.player.fishing != null && ((FishingBobberEntityAccessorMixin) client.player.fishing).getBiting()) {
+            useRod();
+            reeledIn = true;
             delay = 0;
-            hookCastOut = false;
+        }
+
+        if (!reeledIn) {
+            return;
+        }
+
+        if (delay > recastDelayTicks) {
+            useRod();
+            reeledIn = false;
+            delay = 0;
+        } else {
+            delay++;
         }
     }
 
-    public void UseRod() {
+    public void useRod() {
+        switch (FishingRodMode) {
+            case FishingRodModes.fishingRodUnprotected:
+                client.player.swing(InteractionHand.MAIN_HAND);
+                client.gameMode.useItem(client.player, InteractionHand.MAIN_HAND);
+                break;
+            case FishingRodModes.fishingRodProtected:
+                if (client.player.getMainHandItem().getDamageValue() <= client.player.getMainHandItem().getMaxDamage() - 4) {
+                    client.player.swing(InteractionHand.MAIN_HAND);
+                    client.gameMode.useItem(client.player, InteractionHand.MAIN_HAND);
+                }
+                break;
+            case FishingRodModes.allInHotbar:
+                client.player.swing(InteractionHand.MAIN_HAND);
+                client.gameMode.useItem(client.player, InteractionHand.MAIN_HAND);
+                if (reeledIn) {
+                    break;
+                }
+                if (isFishingRodEquipped() && client.player.getMainHandItem().getDamageValue() + 1 != client.player.getMainHandItem().getMaxDamage()) {
+                    break;
+                }
+                int currentSlot = client.player.getInventory().getSelectedSlot();
+                for (int i = 0; i < 9; i++) {
+                    if (isFishingRodEquipped(client.player.getInventory().getItem(i)) && currentSlot != i) {
+                        client.player.getInventory().setSelectedSlot(i);
+                        break;
+                    }
+                }
+        }
+    }
 
-        if (!simpleautofishingMode.modeCheck()) {
-            return;
-        }
-        if (Minecraft.getInstance().player.getMainHandItem().is(fishingRod)) {
-            Minecraft.getInstance().player.swing(InteractionHand.MAIN_HAND);
-            Minecraft.getInstance().gameMode.useItem(Minecraft.getInstance().player, InteractionHand.MAIN_HAND);
+    public static boolean isFishingRodEquipped() {
+        if (client.getInstance().player.getMainHandItem().is(TagKey.create(Registries.ITEM, Identifier.parse("c:tools/fishing_rod")))) {
+            return true;
+        } else if (client.getInstance().player.getMainHandItem().getItem() == Items.FISHING_ROD) {
+            return true;
         } else {
-            Minecraft.getInstance().player.swing(InteractionHand.OFF_HAND);
-            Minecraft.getInstance().gameMode.useItem(Minecraft.getInstance().player, InteractionHand.OFF_HAND);
+            return false;
         }
+    }
+
+    public static boolean isFishingRodEquipped(ItemStack stack) {
+        if (stack.is(TagKey.create(Registries.ITEM, Identifier.parse("c:tools/fishing_rod")))) {
+            return true;
+        } else if (stack.getItem() == Items.FISHING_ROD) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean attackKeyReleased(boolean currentState) {
+        boolean fallingEdge = stateAttackKeyReleased && !currentState;
+        stateAttackKeyReleased = currentState;
+        return fallingEdge;
     }
 
 }
